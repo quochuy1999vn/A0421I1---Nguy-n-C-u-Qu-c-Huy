cfd13a17a14d3d9c6498fee17bd370714315659a
your name <abc@gmail.com>
2021-12-31 20:56:02 +0700

    31/12

--- module 4/bai_10_session_and_cookie/bai_tap/complete_shopping_cart/src/main/java/com/complete_shopping_cart/controller/ProductController.java
+++ module 4/bai_10_session_and_cookie/bai_tap/complete_shopping_cart/src/main/java/com/complete_shopping_cart/controller/ProductController.java
@@ -6,10 +6,7 @@ import com.complete_shopping_cart.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -33,17 +30,40 @@ public class ProductController {
         return modelAndView;
     }
 
-    @GetMapping("create")
+    @GetMapping("/create")
     public String showFormCreate(@ModelAttribute("cart") HashMap<Long, Cart> hashMap, Model model) {
-        System.out.println(hashMap.keySet());
+        //System.out.println(hashMap.keySet());
         model.addAttribute("products", new Product());
-        return "create";
+        return "/create";
     }
 
-    @PostMapping("create")
+    @PostMapping("/create")
     public String saveProduct(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
         productService.save(product);
         redirectAttributes.addAttribute("message", "New product created successfully");
         return "redirect:/shop";
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showFormDelete(@PathVariable Long id) {
+        Product product = productService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("/delete");
+        modelAndView.addObject("products", product);
+        return modelAndView;
+    }
+
+    @PostMapping("/delete/{id}")
+    public ModelAndView deleteProduct(@ModelAttribute Product product, @PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/shop");
+        productService.remove(id);
+        return modelAndView;
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView showFormView(@PathVariable Long id) {
+        Product product = productService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("/view");
+        modelAndView.addObject("products", product);
+        return modelAndView;
     }
 }
